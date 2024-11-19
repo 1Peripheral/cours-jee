b293de27e4398b3412ba9affb6e60c4332b43072
1Peripheral <codels@protonmail.com>
2024-11-19 21:52:33 +0100

    working on billing service

---
+++ ecom-app/billing-service/src/main/java/com/peri/billingservice/models/Product.java
@@ -0,0 +1,11 @@
+package com.peri.billingservice.models;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private int quantity;
+}
